9f41c757898ca1782bda239b9097df2bfe9dd368
LeanNSP <58130895+LeanNSP@users.noreply.github.com>
2021-03-17 15:14:40 +0200

    adds server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const express = require('express');
+const colors = require('colors');
+
+const { NODE_ENV, PORT } = require('./config/env.keys');
+
+const app = express();
+
+// Body parser
+app.use(express.urlencoded());
+app.use(express.json());
+
+const server = app.listen(PORT || 3300, () => {
+  console.log(`Server started in ${NODE_ENV} mode on port: ${PORT}`.black.bgWhite.bold);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
